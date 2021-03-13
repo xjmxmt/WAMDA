86542d2f2ea649523d0a819d583bdb355601d1e8
xjmxmt <42826825+xjmxmt@users.noreply.github.com>
2021-03-13 11:53:51 +0100

    Add files via upload

---
+++ util/OfficeHomeDataset.py
@@ -0,0 +1,54 @@
+import os
+from torch.utils.data import Dataset, DataLoader
+import torch
+import skimage.io as io
+from skimage.transform import resize
+import numpy as np
+
+
+class OfficeHomeDataset(Dataset):
+    def __init__(self, data_path, f_dim=256, c_dim=256):
+        self.f_dim = f_dim
+        self.c_dim = c_dim
+
+        # label dict
+        self.label_dict = {"Art": 0, "Clipart":1, "Product":2}
+
+        # Read all file names in __init__
+        self.file_names = []
+        for root, dirs, files in os.walk(data_path):
+            for filename in files:
+                if filename == ".DS_Store": continue
+                self.file_names.append(os.path.join(root, filename))
+
+    def __len__(self):
+        return len(self.file_names)
+
+    def __getitem__(self, idx):
+        if torch.is_tensor(idx):
+            idx = idx.tolist()
+
+        label = []
+        filename = self.file_names[idx]
+        img = io.imread(filename, as_gray=True)  # np array
+        img = resize(img, (self.f_dim, self.c_dim), anti_aliasing=True)
+        print(img.shape, filename)
+        source_name = filename.split('/')[-3]
+        label.append(self.label_dict[source_name])
+        label = np.array(label)
+        sample = {'image': img, 'label': label}
+
+        return sample
+
+
+if __name__ == "__main__":
+    dataset = OfficeHomeDataset("../dataset/Source")
+    print(len(dataset.file_names))
+    print(dataset.file_names)
+
+    dataloader = DataLoader(dataset, batch_size=4,
+                            shuffle=True, num_workers=0)
+
+    for i_batch, sample_batched in enumerate(dataloader):
+        print(i_batch, sample_batched['image'].size(),
+              sample_batched['label'].size())
